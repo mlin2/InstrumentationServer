9bb69843b77b2221c1bb44723154fde44346e770
sebastianweitzel <sebastian.weitzel@tum.de>
2016-01-29 16:34:54 +0100

    Return Json not found error if not able to get logo data

--- src/main/java/org/sentinel/instrumentationserver/resource/impl/MetadataResourceImpl.java
+++ src/main/java/org/sentinel/instrumentationserver/resource/impl/MetadataResourceImpl.java
@@ -1,5 +1,6 @@
 package org.sentinel.instrumentationserver.resource.impl;
 
+import org.sentinel.instrumentationserver.generated.model.Error;
 import org.sentinel.instrumentationserver.generated.model.MetadataList;
 import org.sentinel.instrumentationserver.generated.workaround.MetadataResource;
 import org.sentinel.instrumentationserver.metadata.MetadataDAO;
@@ -9,9 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-/**
- * Created by sebastian on 1/22/16.
- */
+
 public class MetadataResourceImpl implements MetadataResource {
     @Override
     public GetMetadataAllResponse getMetadataAll() throws Exception {
@@ -38,6 +37,9 @@ public class MetadataResourceImpl implements MetadataResource {
     public GetMetadataLogoByApkHashResponse getMetadataLogoByApkHash(@PathParam("apkHash") String apkHash) throws Exception {
         MetadataDAO metadataDAO = MetadataDAO.getInstance();
         byte[] logoFile = metadataDAO.retrieveLogoFromDatabase(apkHash);
+        if (logoFile.length == 0) {
+            return GetMetadataLogoByApkHashResponse.withJsonNotFound(new Error().withMsg("APK logo file not stored in the database"));
+        }
         return GetMetadataLogoByApkHashResponse.withFormdataOK(logoFile);
     }
 }
