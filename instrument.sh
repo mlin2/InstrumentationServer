f5400e76a955190a738b6cd64bc22baf3c1dbb99
Sebastian Weitzel <mlin2@users.noreply.github.com>
2016-01-31 15:46:16 +0100

    Update instrument.sh

--- instrument.sh
+++ instrument.sh
@@ -16,9 +16,11 @@ cd InstrumentationDependencies
 pwd
 
 ls
-# "m" as a suffix of the timeout argument tells timeout that the time is in minutes. The if statement checks whether the last command was executed successfully and returned status code 0 and only then executes the signing and aligning of the APK.
+# "m" as a suffix of the timeout argument tells timeout that the time is in minutes.
 echo timeout ${13}m java -jar DroidForce.jar -sourceFile $1 -sinkFile $2 -taintWrapper $4 -apkFile $3 -o $5 -j -androidJar $6
 /usr/bin/timeout ${13}m java -jar DroidForce.jar -sourceFile $1 -sinkFile $2 -taintWrapper $4 -apkFile $3 -o $5 -j -androidJar $6
+# Check whether the last command was executed successfully and has returned status code 0 and only then execute
+# the signing and aligning of the APK.
 if [ $? == 0 ]; then
 
 echo The APK gets now signed
